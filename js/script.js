afd0ab2bafa942238bc8afd339c481fec9144846
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-09-18 09:57:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,4 +54,11 @@ function jogarNovamente() {
     mostraPergunta();
 }
 
+function substituiNome() {
+    for (const pergunta of perguntas) {
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
+
+substituiNome();
 mostraPergunta();
